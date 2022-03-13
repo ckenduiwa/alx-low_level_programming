79699ee87659ec26e28b3eddb28883b8b90c9a77
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-13 13:09:03 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,13 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		if (n > 0)
-			printf("%i is positive\n", n)	
+
+		 	printf("%i is positive\n", n)	
 		else if (n < 0)
 			print ("%i is negative\n", n);
+
 		else								
 			printf("%i is zero\n", n)
 
-		return (0);
+		return (0)i;
 }
