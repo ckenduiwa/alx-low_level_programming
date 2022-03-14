aacbabca0dfe3dbb7edf32bc903ee89a23311445
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-14 13:43:51 -0700

    last

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,9 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		if (n > 0)
+		last = n % 10;
+	       
+		if (last > 5)
 			printf("%i is positive\n", n);
 		else if (n < 0)
 			printf("%i is negative\n", n);
