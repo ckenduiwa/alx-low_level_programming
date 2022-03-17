42ce4a22e8114be52bc7b7580b3bccc25957585d
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-17 12:27:50 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
+int main(void)
+{
+	int i;
 
+	i = 98;
+	positive_or_negative(i);
 
-#include <stdio.h>
-
-
-
-void positive_or_negative(int i);
+	return (0);
+}
