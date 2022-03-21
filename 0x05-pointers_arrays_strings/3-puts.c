1db9907d0efa821b83d43472a972a0b36bb5f364
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-21 15:40:36 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *
+ * @str: string to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	int iter = 0;
+
+	while (str[iter])
+	{
+		_putchar(str[iter]);
+		iter++;
+	}
+	_putchar(10);
+}
