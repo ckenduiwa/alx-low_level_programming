af3bc400d8c6ad76e64c06affbe429e7ccf382bd
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-21 15:53:01 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - takes string input as a parameter
+ *
+ * @s: string to be taken
+ * Return: string reversed
+ */
+
+void rev_string(char *s)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < _strlen(s) / 2; a++)
+	{
+		b = s[a];
+		s[a] = s[_strlen(s) - a - 1];
+		s[_strlen(s) - a - 1] = b;
+	}
+}
