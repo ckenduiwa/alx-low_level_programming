f030e7fe854fe39938bbe6e756486ef9a43846f6
ckenduiwa <kenduiwacollins@gmail.com>
2022-03-24 13:47:32 -0700

    zero

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,27 @@
 
 /**
  * _strcat - concatenates two strings
- *
- * @dest: pointer to string 1
+* @dest: pointer to string 1
  * @src: pointer to string 2
- *
  * Return: pointer to a concatenated string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int a = 0;
-	int b;
+	int cl = 0, c2 = 0;
 
-	while (dest[a])
-		a++;
+	while (*(dest + c1) != '\0')
+	{
+		c1++;
+	}
 
-	for (b = 0; src[b] != '\0'; b++, a++)
-		dest[a] = src[b];
-
-	dest[a] = '\0';
-
-	return (dest);
+	while (c2 >= 0)
+	{
+		*(dest + c1) = *(src + c2);
+		if (*(src + c2) == '\0')
+			break;
+		c1++;
+		c2++;
+	}
+	return (dest)
 }
