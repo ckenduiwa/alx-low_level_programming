c1f916b78f3fa5dbe1d73c94e4df21562499993c
ckenduiwa <kenduiwacollins@gmail.com>
2022-04-07 15:03:04 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,83 +1,38 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 /**
+ * str_concat - concatenates two strings using malloc
  *
- *  * str_concat - concatenates strings
+ * @s1: string 1
+ * @s2: string 2
  *
- *   * @s1: first string
- *
- *    * @s2: second string
- *
- *     * Return: pointer to concatenated string
- *
- *      */
-
-
+ * Return: pointer to cancatenated string
+ */
 
 char *str_concat(char *s1, char *s2)
-
 {
-
-		char *s3;
-
-			unsigned int i, j, c, p, size;
-
-
-
-				if (s1 == NULL)
-
-							s1 = "";
-
-					if (s2 == NULL)
-
-								s2 = "";
-
-
-
-						for (i = 0; s1[i] != '\0'; i++)
-
-									;
-
-
-
-							for (j = 0; s2[j] != '\0'; j++)
-
-										;
-
-
-
-								size = i + j;
-
-									s3 = (char *)malloc(sizeof(char) * (size + 1));
-
-
-
-										if (s3 == NULL)
-
-												{
-
-															free(s3);
-
-																	return (NULL);
-
-																		}
-
-
-
-											for (c = 0; c < i; c++)
-
-														s3[c] = s1[c];
-
-												p = j;
-
-													for (j = 0; j <= p; c++, j++)
-
-																s3[c] = s2[j];
-
-
-
-														return (s3);
-
+	char *ptr;
+	int a, b, c, d;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a = 0; *(s1 + a) != '\0'; a++)
+		;
+	for (b = 0; *(s2 + b) != '\0'; b++)
+		;
+	
+	ptr = malloc((a * sizeof(*s1)) + (b * sizeof(*s2)) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (c = 0, d = 0; c < (a + b + 1); c++)
+	{
+		if (c < a)
+			ptr[c] = s1[c];
+		else
+			ptr[c] = s2[d++];
+	}
+	
+	return (ptr);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,81 +1,40 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 /**
- *
- *  * alloc_grid - creates a 2d integer grid
- *
- *   * @width: number of columns
- *
- *    * @height: number of rows
- *
- *     * Return: pointer to grid or NULL
- *
- *      */
-
-int **alloc_grid(int width, int height)
-
+ * alloc_grid - creates a 2d integer grid
+ * @width: number of columns
+ * @height: number of rows
+ * Return: pointer to grid or NULL
+ */
+int **alloc_grid(int width, int height)	
 {
+	int **t;
+	int i, j;
 
-		int **t;
+	if (width < 1 || height < 1)
+		return (NULL);
 
-			int i, j;
+	t = malloc(height * sizeof(int *));
+	if (t == NULL)
+	{
+		free(t);
+		return (NULL);
+		}
 
-
-
-				if (width < 1 || height < 1)
-
-							return (NULL);
-
-
-
-					t = malloc(height * sizeof(int *));
-
-						if (t == NULL)
-
-								{
-
-											free(t);
-
-													return (NULL);
-
-														}
-
-
-
-							for (i = 0; i < height; i++)
-
-									{
-
-												t[i] = malloc(width * sizeof(int));
-
-														if (t[i] == NULL)
-
-																	{
-
-																					for (i--; i >= 0; i--)
-
-																										free(t[i]);
-
-																								free(t);
-
-																											return (NULL);
-
-																													}
-
-															}
-
-
-
-								for (i = 0; i < height; i++)
-
-											for (j = 0; j < width; j++)
-
-															t[i][j] = 0;
-
-
-
-									return (t);
-
+	for (i = 0; i < height; i++)
+	{
+		t[i] = malloc(width * sizeof(int));
+		if (t[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(t[i]);
+			free(t);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			t[i][j] = 0;
+	
+	return (t);
 }
