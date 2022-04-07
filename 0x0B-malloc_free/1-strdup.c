dabaac52158bb6a84abd2cb59b5973c72a7ae3c9
ckenduiwa <kenduiwacollins@gmail.com>
2022-04-07 14:31:26 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,57 +1,28 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdlib.h>
 
 /**
+ * _strdup - duplicates a string
  *
- *  * _strdup - points to a duplicate string
+ * @str: size of array
  *
- *   * @str: the string to compare
- *
- *    * Return: pointer to the duplicate or NULL
- *
- *     */
+ * Return: NULL or pointer to arrar
+ */
 
 char *_strdup(char *str)
-
 {
+	char *ptr;
+	int iter = 0;
+	int mem;
 
-		char *m;
-
-			unsigned int i, j;
-
-
-
-				if (str == NULL)
-
-							return (NULL);
-
-
-
-					for (j = 0; str[j] != '\0'; j++)
-
-								;
-
-
-
-						m = (char *)malloc(sizeof(char) * (j + 1));
-
-
-
-							if (m == NULL)
-
-										return (NULL);
-
-
-
-								for (i = 0; i <= j; i++)
-
-										{
-
-													m[i] = str[i];
-
-														}
-
-									return (m);
-
+	if (str == NULL)
+		return (NULL);
+	while (*(str + iter))
+		iter++;
+	ptr = malloc(iter * sizeof(char) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (mem = 0; mem < iter; mem++)
+		ptr[mem] = str[mem];
+	return (ptr);
 }
