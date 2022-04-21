76749b077f4352eee467faad1368a572de70354a
ckenduiwa <kenduiwacollins@gmail.com>
2022-04-21 14:37:58 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees a list from memory
+ * @head: pointer to beginning of list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
