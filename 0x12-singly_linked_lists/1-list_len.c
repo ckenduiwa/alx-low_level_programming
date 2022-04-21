fceb7cc6efb8e6be2d0ba7ebac930003f745ad4d
ckenduiwa <kenduiwacollins@gmail.com>
2022-04-21 14:27:03 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - eturns number of elements in a list
+ * @h: pointer to the list
+ * Return: length of list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
