c57fe0ec46860d4a8a779c04b6bc5e685caded8e
ckenduiwa <kenduiwacollins@gmail.com>
2022-05-04 15:42:37 -0700

    champ

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,33 @@
-#include <stdlibh>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
+ * create_file - creates a file.
+ * @filename: a pointer to the name of the file to create.
+ * @text_content: a pointer to a string to write to the file.
  *
- * Return: length of the strinf
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-/**
- * create_file - creates file
- * @filename: pointer to file
- * @text_content: contents in the file
- * Return: 1 on success, -1 on failure
+ * Return: if the function fails - - 1. Otherwise -1
+ *
+ *
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (len == -1)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
+
+	close(fd);
+
 	return (1);
 }
